4e5b2d6a1d8187c97721e1aca74974a17b1426c6
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-10-12 01:00:40 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Statistics = () => {
+  const { data } = useLoaderData();
   return (
     <div>
       <h2>Statistics page</h2>
+      <ResponsiveContainer width="95%" height={300}>
+        <LineChart width={1000} height={300} data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="total"
+            stroke="#00aaff"
+            strokeWidth={2}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
