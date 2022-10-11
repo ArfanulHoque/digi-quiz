307cadf4f0707e06c037356b42c82bb880090548
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-10-12 00:31:13 +0600

    condition added

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,16 +1,43 @@
 import React from "react";
 import "./Questions.css";
+import { EyeIcon } from "@heroicons/react/24/solid";
+import { toast } from "react-toastify";
 
-const Questions = ({ question }) => {
-  const { questions } = question;
+const Questions = ({ question, _idx }) => {
+  const correct = () => toast.success("Your Answer is Correct");
+  const wrong = () => toast.error("Your Answer is Wrong");
+  //   const { questions } = question;
   const { options, correctAnswer } = question;
+  const answer = () => toast.info(`Your answer is: ${correctAnswer}`);
+  const getAnswer = (data) => {
+    if (data === correctAnswer) {
+      correct();
+    } else {
+      wrong();
+    }
+  };
+  const showAnswer = () => {
+    answer();
+  };
   return (
     <div className="question">
+      <div>
+        <h5>
+          Quiz {_idx + 1} : {question.length}
+        </h5>
+        <EyeIcon onClick={showAnswer} className="eye" />
+      </div>
       <h4>{question.question}</h4>
+
       <div className="option">
         {options.map((option, _idx) => (
           <label key={_idx}>
-            <input type="radio" />
+            <input
+              type="radio"
+              name="q"
+              onClick={() => getAnswer(option)}
+              id={_idx}
+            />
             {option}
           </label>
         ))}
